93a3679000ca7aa8c6ce350b81b41e8c895d004e
BABITA-05 <bhandaribabita383@gmail.com>
2026-03-02 22:35:50 +0545

    learnt just little

--- dictionary-loop.py
+++ dictionary-loop.py
@@ -8,3 +8,9 @@ for k, v in fruits.items():
 for i, v in enumerate(['tic', 'tac', 'toe']):
     print(i, v)
 
+
+#To loop over two or more sequences at the same time, the entries can be paired with the zip() function.
+questions = ['name', 'quest', 'favourite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+    print('What is your {0}? It is {1}.'.format(q, a))
